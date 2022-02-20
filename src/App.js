ed8fd8693b3102926d58250b62591925d075dd0f
KateSlav7 <85428845+KateSlav7@users.noreply.github.com>
2022-02-20 20:28:09 +0200

    commit04

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ function App() {
   return (
     <div>
       <h1>Kate Slavgorodskaya</h1>
+      <h2>Kate Slavgorodskaya</h2>
+      <h3>Kate Slavgorodskaya</h3>
+      <h4>Kate Slavgorodskaya</h4>
     </div>
   );
 }
